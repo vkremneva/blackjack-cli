431808e0f7edd3fc9be6a9fda46b32f8199724ba
Veronika Kremneva <verkremneva@gmail.com>
2024-09-19 20:17:53 +0300

    cardlist.h: added prototype

---
+++ cardlist.h
@@ -0,0 +1,34 @@
+#ifndef CARDLIST_H
+#define CARDLIST_H
+
+#include <stddef.h>
+
+#include "card.h"
+#include "status.h"
+
+/**
+ * @brief A structure to represent a deck of cards: linked list of card_t nodes.
+ *
+ * @param head A pointer to the head of the list, i.e. first card in the deck.
+ * @param len An amount of nodes in the list, i.e. amount of cards in the deck.
+ */
+
+typedef struct cardlist_t 
+{
+    struct card_t *head; 
+    size_t len;
+
+} cardlist_t;
+
+status_t initEmptyDeck(cardlist_t **deck);
+status_t initFullDeck(cardlist_t **deck);
+
+status_t pushFront(cardlist_t **deck, card_t *card);
+status_t popFront(cardlist_t **deck, card_t *card);
+status_t popByInd(cardlist_t **deck, card_t *card, size_t ind);
+
+status_t getDeckScoreValue(cardlist_t **deck, int8_t *score);
+
+status_t freeDeck(cardlist_t **deck);
+
+#endif // CARDLIST_H
